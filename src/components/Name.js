876db99a55235015b42e07c4befafbc9a089c049
jhslashdb <109535123+jhslashdb@users.noreply.github.com>
2022-10-12 11:53:53 -0400

    Name component fix
    - fixed bug where cursor jumped to end of line when editing task/list names

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
+import { useState } from 'react';
+
 export default function Name(props) {
   let { style, fieldName, fieldValue, update, path } = props;
+
+  const [value, setValue] = useState(fieldValue);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.target.blur();
+    }
+  }
+
+  const updateValue = async (e) => {
+    fieldValue = e.target.value;
+    await update(path, { [fieldName]: `${fieldValue}`})
+  }
+
   return (
     <input
       type="textarea"
       style={style}
       className="field"
-      value={fieldValue}
-      onChange={async (e) => {
-        fieldValue = e.target.value;
-        await update(path, { [fieldName]: `${fieldValue}`})
+      value={value}
+      onChange={e => {
+        setValue(e.target.value);
+      }}
+
+      onKeyDown={ e => {
+        handleKeyDown(e);
+      }}
+
+      onBlur={ e => {
+        updateValue(e);
       }}
     ></input>
   );
