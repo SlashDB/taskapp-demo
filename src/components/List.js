5d0de49d4510e2332612e7678d12678bd62baec1
jhslashdb <109535123+jhslashdb@users.noreply.github.com>
2022-10-27 06:01:58 -0400

    removed old comments

--- src/components/List.js
+++ src/components/List.js
@@ -10,22 +10,8 @@ const List = (props) => {
   const { TaskListId, list, getList, postList, putList, deleteList } = props;
   const [task, setTask] = useState('');
 
-  //const location = ['TaskList', 'TaskListId', `${TaskListId}`];
-  //const path = `TaskListId/${TaskListId}?stream=true`;
-
-  // const demoClient = useSetUp('demo', 'https://demo.slashdb.com','taskapp','wwv7nppvsj147rhdbi5mnm1zm8risb53');
-  // const taskListName = new DataDiscoveryFilter(eq('TaskListId',TaskListId));
-  // const [demotasks, demogetTasks, demopostTask, demoputTask, demodeleteTask] = useDataDiscovery(
-  //    'taskdatadb',
-  //    'TaskItem', 
-  //    taskListName,
-  //    'demo'
-  //  );
-  // console.log(demotasks);
-
   const taskListIDPath = new DataDiscoveryFilter(eq('TaskListId',TaskListId));
   const queryParams = new SQLPassThruFilter({'TaskListId':TaskListId});
-
 
   const [tasks, getTasks, postTask, putTask, deleteTask] = useDataDiscovery(
     process.env.REACT_APP_DATABASE_NAME,
