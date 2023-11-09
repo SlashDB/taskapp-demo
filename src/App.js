4e04bd01e45a569fc283dd5b66e71b35cba2d715
Nelson Carrasquel <carrasquel@outlook.com>
2023-11-09 18:08:07 -0400

    function

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import { Route, Switch } from 'react-router-dom';
 import { ProtectedRoute } from './ProtectedRoute';
 import { useSetUp } from '@slashdb/react-slashdb';
 
-function App() {
+// function App() {
+const App = () =>
+{
 
-  useSetUp();
+  const host = "http://host.docker.internal:8000";	// set SlashDB host here
+	const username = "taskapp";	// set SlashDB username here
+	const apiKey = "wwv7nppvsj147rhdbi5mnm1zm8risb53";	// set SlashDB API key here
+	
+  const config = {
+		host: host,
+		username: username,
+		apiKey: apiKey
+	}
+
+  // useSetUp();
+  useSetUp('default', config);
   
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -3,27 +3,19 @@
  * Wrap children with router and provide params for SlashDBProvider.
  *
  */
- import React from 'react';
- import ReactDOM from 'react-dom/client';
- import App from './App';
- import { HashRouter } from 'react-router-dom';
- 
- import { SlashDBProvider } from '@slashdb/react-slashdb';
- 
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(
-   <React.StrictMode>
-   <HashRouter basename="/">
-     <SlashDBProvider
-       baseUrl={process.env.REACT_APP_SLASHDB_SERVER_URL}
-       setUpOptions={{
-         username: process.env.REACT_APP_DATABASE_USERNAME,
-         //password: process.env.REACT_APP_DATABASE_PASSWORD,
-         apiKey: process.env.REACT_APP_USER_API_KEY,
-       }}
-     >
-     <App />
-     </SlashDBProvider>
-   </HashRouter>,
-   </React.StrictMode>
- );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
+
+import { SlashDBProvider } from '@slashdb/react-slashdb';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+  <HashRouter basename="/">
+    <App />
+  </HashRouter>,
+  </React.StrictMode>
+);
