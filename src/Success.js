2d1707e271a3a9819561d1415f2808376761b8c2
Nelson Carrasquel <carrasquel@outlook.com>
2023-11-09 22:54:30 -0400

    Success page added

---
+++ src/Success.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+
+export default function Success(props) {
+    return (
+        <div>
+          <p>Login was successful, you can close this window...</p>
+        </div>
+    );
+}
