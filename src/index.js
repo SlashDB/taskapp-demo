de8b2de7be43774d4dda027cb850f039dfc15d31
Nelson Carrasquel <carrasquel@outlook.com>
2023-11-21 09:18:20 -0400

    unused import removed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 import { SlashDBProvider } from '@slashdb/react-slashdb';
 
